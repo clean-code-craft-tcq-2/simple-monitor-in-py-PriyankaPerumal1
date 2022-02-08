bf7d97cecc27a97957f1a3c49f58852c92f547a4
PriyankaPerumal1 <90901203+PriyankaPerumal1@users.noreply.github.com>
2022-02-08 19:21:16 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -1,18 +1,32 @@
+import BMS_range
 
+class CheckLimit:
+  def __init__(self, value, range):
+    self.value = value
+    self.range = range
+  
+  def check_limit(self):
+    withinLimit = False
+    if self.value < self.range.max_limit:
+      print(self.range.parameter + ' is below minimum limit!')
+    elif self.value > self.range.max_limit:
+      print(self.range.parameter + ' is exceeding the maximum limit!')
+    else:
+      withinLimit = True
+      
+    return withinLimit
+  
 def battery_is_ok(temperature, soc, charge_rate):
-  if temperature < 0 or temperature > 45:
-    print('Temperature is out of range!')
-    return False
-  elif soc < 20 or soc > 80:
-    print('State of Charge is out of range!')
-    return False
-  elif charge_rate > 0.8:
-    print('Charge rate is out of range!')
-    return False
+  temperature_check = CheckLimit(temperature, BMSRange.temperature_range)
+  soc_check = CheckLimit(soc, BMSRange.soc_range)
+  charge_rate_check = CheckLimit(charge_rate, BMSRange.charge_rate_range)
+  
+  return temperature_check.check_limit() and soc_check.check_limit() and charge_rate_check.check_limit()
 
-  return True
-
-
-if __name__ == '__main__':
+def test_battery():
   assert(battery_is_ok(25, 70, 0.7) is True)
   assert(battery_is_ok(50, 85, 0) is False)
+  assert(battery_is_ok(20, 60, 0.9) is False)
+  
+if __name__ == '__main__':
+    test_battery()
